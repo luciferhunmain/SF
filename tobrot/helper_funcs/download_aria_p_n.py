88d9389731da611062c14c4382bd1f808cff8757
luciferhunmain <54854974+luciferhunmain@users.noreply.github.com>
2021-04-29 19:54:08 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -281,7 +281,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 else:
                     msgg = f"<b>Torrent Details</b>: <b>S</b>: {file.num_seeders} <b>L</b>: {file.connections}"
                 msg = f"\n<b>Name</b>: `{downloading_dir_name}`"
-                msg += f"\n<b>Speed</b>: {file.download_speed_string()}\n"
+                msg += f"\n<b>Speed</b>: {file.download_speed_string()}"
                 msg += f"\n<b>Status</b>: {file.completed_length_string()} <b>of</b> {file.total_length_string()} \n<b>ETA</b>: {file.eta_string()} \n{msgg}"
                 # msg += f"\nSize: {file.total_length_string()}"
 
